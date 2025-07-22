9a514486ee9695852c9ff5799216912c520edfd3
Shaik Arshadh kareem <shaikarshadhh@gmail.com>
2025-07-22 17:24:29 +0530

    Initial commit

---
+++ ex.py
@@ -0,0 +1,10 @@
+# This program takes your name as input and greets you.
+
+# Get input from the user
+name = input("What's your name? ")
+
+# Print a greeting
+print(f"Hello, {name}!")
+
+# Print a fixed message
+print("Welcome to Python!")
